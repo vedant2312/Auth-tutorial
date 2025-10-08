780bc02bf8054c2aa48bbc9169249a67880792d0
Vedant Rele <vedant2312@gmail.com>
2025-10-08 07:52:26 +0530

    Initial project setup along with db setup

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import { connectDB } from './db/connectDB.js';
+import authRoutes from './routes/auth.route.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World 123");
+});
+
+app.get("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
