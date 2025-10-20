74a79562dba4cb2d242bbe8c82416721949f8d20
Vedant Rele <vedant2312@gmail.com>
2025-10-20 10:03:12 +0530

    Email verification is added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -47,6 +47,45 @@ export const signup = async (req, res) => {
     }
 };
 
+export const verifyEmail = async (req, res) => {
+    // user will enter the token they received in email
+    const { code } = req.body;
+
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() }, // token not expired
+        });
+
+        if(!user) {
+            return res.status(400).json({success: false, message: "Invalid or expired verification code"});
+        }
+
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+        await user.save();
+
+        await sendWelcomeEmail(user.email, user.name);
+
+        res.status(200).json({
+            success: true, 
+            message: "Email verified successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            },
+        });
+    } catch (error) {
+        console.log("Error in verifyEmail ", error);
+
+        res.status(500).json({
+            success: false,
+            message: "Server Error",
+        });
+    }
+};
+
 export const login = async (req, res) => {
     res.send("Login route");
 };
